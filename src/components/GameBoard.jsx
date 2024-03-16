a23404212863d736b1c02a6235d1c95d1cced747
dangminh214 <dangminh214@gmail.com>
2024-03-16 19:39:27 +0100

    now the turn changes after each player moves

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -5,19 +5,20 @@ const initialGameBoard  = [
   [null, null, null]
 ];
 
-export default function GameBoard() {
+export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   function handleSelectSquare(rowIndex, colIndex, ) {
     setGameBoard((prevGameBoard) => {
       const updateBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-
-      updateBoard[rowIndex][colIndex] = 'X';
+      updateBoard[rowIndex][colIndex] = activePlayerSymbol;
       return updateBoard;
     });
+    onSelectSquare();
   }
 
-  return (<ol id = "game-board">
+  return (
+  <ol id = "game-board">
     {gameBoard.map((row, rowIndex) => (
       <li key={rowIndex}>
         <ol>
